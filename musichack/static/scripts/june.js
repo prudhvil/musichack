2e6617dccf4255b0fe059109b92d6074ac603f61
Sharad Vikram <sharad.vikram@gmail.com>
2015-10-04 00:35:09 -0700

    travel

--- musichack/static/scripts/june.js
+++ musichack/static/scripts/june.js
@@ -1,56 +1,58 @@
-var fromSong, toSong;
-var songs = []
-function unhideTo() {
-  $("#to").fadeIn(300, function() {
-    $("#to").focus();
-  });
-}
+$(document).ready(function() {
 
-function loadYoutube(songs) {
-  // Load the IFrame Player API code asynchronously.
-  youtubeIds = songs.map(function(s) {return s.youtube});
-  $("#youtubeplayer").html('<iframe width="100%" src="http://www.youtube.com/embed/'+youtubeIds[0]+'?autoplay=1&playlist='+youtubeIds.slice(1).join(',')+'" frameborder="0" allowfullscreen></iframe>')
-}
-
-var songs = []
-
-var clearSongs = function() {
-  songs.splice(0, songs.length);
-}
-
-var getId = function(id, youtube, cb) {
-  youtube = youtube || false;
-  $.ajax('/api/get_id', {
-    dataType: 'jsonp',
-    data: {
-      id: id,
-      youtube: youtube
-    }
-  }).done(function(data) {
-    cb(data.result);
-  })
-}
-
-var addSongToPlaylist = function(id, cb) {
-  getId(id, true, function(song) {
-    songs.push(song);
-    artwork = song.artwork['100'];
-    if (!artwork) {
-      artwork = "/img/no_album_art.png";
-    }
-    var elem = $("<div></div>").loadTemplate($("#playlisttemplate"), {
-      name: song.name,
-      artist: song.artist,
-      artwork: artwork,
-      youtube: "http://www.youtube.com/watch?v="+song.youtube,
+  var fromSong, toSong;
+  var songs = []
+  function unhideTo() {
+    $("#to").fadeIn(300, function() {
+      $("#tosong").focus();
     });
-    $('#playlist').append(elem);
-    cb();
-  });
-}
+  }
+
+  function loadYoutube(songs) {
+    // Load the IFrame Player API code asynchronously.
+    youtubeIds = songs.map(function(s) {return s.youtube});
+    $("#youtubeplayer").html('<iframe width="100%" src="http://www.youtube.com/embed/'+youtubeIds[0]+'?autoplay=1&playlist='+youtubeIds.slice(1).join(',')+'" frameborder="0" allowfullscreen></iframe>')
+  }
+
+  var songs = []
+
+  var clearSongs = function() {
+    songs.splice(0, songs.length);
+  }
+
+  var getId = function(id, youtube, cb) {
+    youtube = youtube || false;
+    $.ajax('/api/get_id', {
+      dataType: 'jsonp',
+      data: {
+        id: id,
+        youtube: youtube
+      }
+    }).done(function(data) {
+      cb(data.result);
+    })
+  }
+
+  var addSongToPlaylist = function(id, cb) {
+    getId(id, true, function(song) {
+      songs.push(song);
+      artwork = song.artwork['100'];
+      if (!artwork) {
+        artwork = "/img/no_album_art.png";
+      }
+      var elem = $("<div></div>").loadTemplate($("#playlisttemplate"), {
+        name: song.name,
+        artist: song.artist,
+        artwork: artwork,
+        youtube: "http://www.youtube.com/watch?v="+song.youtube,
+      });
+      $('#playlist').append(elem);
+      cb();
+    });
+  }
 
 
-function scrollDown() {
+  function scrollDown() {
     $("div#second").removeClass("hidden");
     $("a#logo").removeClass("hidden");
     $("div#graph").removeClass("hidden");
@@ -65,35 +67,41 @@ function scrollDown() {
     $("div#from").detach().prependTo('#second');
 
 
-}
+  }
 
-$('#from').bind('typeahead:autocompleted typeahead:selected', function(obj, datum, name) {
-  fromSong = datum;
-  unhideTo();
-});
-
-var loadAdventureGraph = function(start, end) {
-  var finished = false;
-  Graph.start({id:start}, {id:end}, function(songId) {
-    addSongToPlaylist(songId, function() {
-      if (finished) {
-        loadYoutube(songs);
-      }
-    });
-  }, function() {
-    finished = true;
-  }, function(songId) {
-    $.getJSON("/api/get_id?callback=?&youtube=true&id="+songId, {}, function(data) {
-      loadYoutube([data.result])
-     })
+  $('#fromsong').bind('typeahead:autocompleted typeahead:selected', function(obj, datum, name) {
+    fromSong = datum;
+    unhideTo();
   });
-}
 
-$('#to').bind('typeahead:autocompleted typeahead:selected', function(obj, datum, name) {
-  toSong = datum;
-  scrollDown();
+  var loadAdventureGraph = function(start, end) {
+    $("#graph-header").text("travel");
+    $("#graph-header").removeClass("explore");
+    $("#graph-header").addClass("travel");
+    $("#first").hide()
+    $("#second").show()
+    var finished = false;
+    Graph.start({id:start}, {id:end}, function(songId) {
+      addSongToPlaylist(songId, function() {
+        if (finished) {
+          loadYoutube(songs);
+        }
+      });
+    }, function() {
+      finished = true;
+    }, function(songId) {
+      $.getJSON("/api/get_id?callback=?&youtube=true&id="+songId, {}, function(data) {
+        loadYoutube([data.result])
+      })
+    });
+  }
 
-  loadAdventureGraph(fromSong.id, toSong.id);
+  $('#tosong').bind('typeahead:autocompleted typeahead:selected', function(obj, datum, name) {
+    toSong = datum;
+    //scrollDown();
+
+    loadAdventureGraph(fromSong.id, toSong.id);
+  });
+
+  //loadAdventureGraph(3, 5);
 });
-
-loadAdventureGraph(3, 5);
